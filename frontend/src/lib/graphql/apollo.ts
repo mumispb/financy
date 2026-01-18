3ee58757cb6524b9c78544137400afaee96a3fd3
Murilo <murilo.pereira.botelho@gmail.com>
2026-01-18 13:07:48 -0300

    feat: adds dotenv and removes dev.db from tracking

--- frontend/src/lib/graphql/apollo.ts
+++ frontend/src/lib/graphql/apollo.ts
@@ -4,7 +4,7 @@ import { useAuthStore } from '../../stores/auth'
 
 
 const httpLink = new HttpLink({
-    uri: "http://localhost:4000/graphql"
+    uri: import.meta.env.VITE_BACKEND_URL || "http://localhost:4000/graphql"
 })
 
 const authLink = new SetContextLink((prevContext) => {
